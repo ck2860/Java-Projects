1b459292a98b4f83be9bb910a4368c1a92fd8b1a
ck2860 <54419460+ck2860@users.noreply.github.com>
2021-10-06 09:09:33 -0700

    Add files via upload

---
+++ homework-01-ck2860/src/Carroll.java
@@ -0,0 +1,31 @@
+/* SWEN-601
+* Condy Kan
+* ck2860@rit.edu
+*The purpose of assignment#1 is to create files, learn java programming and use Git/GitHub.
+*/
+
+/* Begin of multi-line comments
+*This code inside of the class's body executes Lewis Carroll's known quote from Alice in Wonderland.
+*The class declaration is public class Carroll
+*The main method is public static void main(String[] args)
+* the main method's body is inside of inner curly braces, which is inside of class's body.
+*system.out.println(""); is a command to print a line with a newline in the end.
+*system.out.print(""); is a command to print without a newline.
+*to create double quotations, we use \" command.
+*to create tabbed spaces, use \t command.
+* End of multi-line comments
+*/
+
+public class Carroll {
+    public static void main(String[] args) {
+        System.out.println("\"Begin at the");
+        System.out.print("beginning,\"");
+        System.out.println(", the King");
+        System.out.print("said, very gravely, \"");
+        System.out.println("and");
+        System.out.println("go on till you come to");
+        System.out.println("the end: then stop.\"");
+        System.out.println(" \t\t  -Lewis Carroll");
+    }
+}
+// end of Carroll Java Class
